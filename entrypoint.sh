cbe6f2c570debce51c34c2e0413e82e99e4623df
Milord-ThatOneModder <100714051+Milord-ThatOneModder@users.noreply.github.com>
2023-11-26 23:29:12 +0100

    Initial Commit

---
+++ entrypoint.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+
+# Wait for the container to fully initialize
+sleep 1
+
+# Default the TZ environment variable to UTC.
+TZ=${TZ:-UTC}
+export TZ
+
+# Set environment variable that holds the Internal Docker IP
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP
+
+
+# Switch to the container's working directory
+cd /home/container || exit 1
+
+## just in case someone removed the defaults.
+if [ "${STEAM_USER}" == "" ]; then
+    echo -e "steam user is not set.\n"
+    echo -e "Using anonymous user.\n"
+    STEAM_USER=anonymous
+    STEAM_PASS=""
+    STEAM_AUTH=""
+else
+    echo -e "user set to ${STEAM_USER}"
+fi
+
+## if auto_update is not set or to 1 update
+if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then 
+    # Update Source Server
+    wget -N https://github.com/Nova-Atomic/Rimworld-Together/releases/download/1.1.1/Linux-x64.zip -O RimworldTogether.zip
+    unzip -qo ModManager.zip
+else
+    echo -e "Not updating game server as auto update was set to 0. Starting Server"
+fi
+
+
+# Replace Startup Variables
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo -e ":/home/container$ ${MODIFIED_STARTUP}"
+
+touch custom_script.sh
+chmod +x custom_script.sh
+
+# Run the Server
+eval ${MODIFIED_STARTUP}
