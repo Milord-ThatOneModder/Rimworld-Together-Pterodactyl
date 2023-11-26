cc6754936627391b4b6fe90677c436a4ff09fe41
Milord-ThatOneModder <100714051+Milord-ThatOneModder@users.noreply.github.com>
2023-11-26 23:54:16 +0100

    Update entrypoint.sh
    fixed typeo in unzip

--- entrypoint.sh
+++ entrypoint.sh
@@ -31,7 +31,7 @@ fi
 if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then 
     # Update Source Server
     wget -N https://github.com/Nova-Atomic/Rimworld-Together/releases/download/1.1.1/Linux-x64.zip -O RimworldTogether.zip
-    unzip -qo ModManager.zip
+    unzip -qo RimworldTogether.zip
 else
     echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
